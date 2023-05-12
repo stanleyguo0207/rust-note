ea1ea23957eb03d22c703ab0f69be9bbb4de029d
stanleyguo0207 <guotongshuai@ztgame.com>
2023-05-12 18:36:33 +0800

    feat: 测试更新

---
+++ README.md
@@ -0,0 +1,31 @@
+Rust 笔记
+---
+
+## Rust安装
+
+### windows
+
+下载地址 `https://www.rust-lang.org/tools/install`
+
+安装文档 `https://rust-lang.github.io/rustup/installation/index.html`
+
+1. 修改环境变量
+
+    - `CARGO_HOME`
+    - `RUSTUP_HOME`
+
+2. 确保在运行`rustup-init.exe`前，修改好环境变量
+
+## IDE
+
+### vscode插件
+
+- [x] [rust-lang.rust-analyzer](https://marketplace.visualstudio.com/items?itemName=rust-lang.rust-analyzer)
+
+## hello-world
+
+```shell
+cargo new hello-world
+cd hello-world
+code .
+```
---
+++ command-line/src/main.rs
@@ -0,0 +1,21 @@
+use clap::Parser;
+
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(Parser)]
+struct Cli {
+    pattern: String,
+    path: std::path::PathBuf,
+}
+
+fn main() {
+    println!("Hello, world!");
+
+    let pattern = std::env::args().nth(1).expect("no pattern given");
+    let path = std::env::args().nth(2).expect("no path given");
+    let args = Cli {
+        pattern: pattern,
+        path: std::path::PathBuf::from(path),
+    };
+
+    println!("args:{} path:{:?}", args.pattern, args.path.as_os_str())
+}
