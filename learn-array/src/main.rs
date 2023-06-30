3667611511a89d6ec11e209e6d75eac38b71091b
stanleyguo0207 <guotongshuai@ztgame.com>
2023-06-30 22:29:04 +0800

    feat: 更新学习仓

---
+++ learn-array/src/main.rs
@@ -0,0 +1,4 @@
+fn main() {
+    let arr = [3; 5];
+    println!("arr = {:?}", arr);
+}
---
+++ learn-tuple/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let tup :(i32, f64, u8) = (500, 6.4, 1);
+    
+    let five_hundred = tup.0;
+    let six_point_four = tup.1;
+    let one = tup.2;
+
+    println!("{five_hundred} {six_point_four} {one}");
+}
