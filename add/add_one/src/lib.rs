e47017771076cf3b8ee3b25982b9a9817d3e2ab6
stanleyguo0207 <stanleyguo0207@163.com>
2023-09-21 16:57:36 +0800

    feat: workspace更新
    Signed-off-by: stanleyguo0207 <stanleyguo0207@163.com>

---
+++ add/add_one/src/lib.rs
@@ -0,0 +1,25 @@
+//! # Adder
+//! `add_one` add one funciton.
+
+/// Adds one to the number given.
+///
+/// # Examples
+///
+/// ```
+/// let arg = 5;
+/// let answer = add_one::add_one(arg);
+///
+/// assert_eq!(6, answer);
+/// ```
+pub fn add_one(x: i32) -> i32 {
+    x + 1
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn it_works() {
+        assert_eq!(3, add_one(2));
+    }
+}
