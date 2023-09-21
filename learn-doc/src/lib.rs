9a08fcfa38518cc110dd12050fa90b0cdb2cf4dd
stanleyguo0207 <stanleyguo0207@163.com>
2023-09-21 16:44:00 +0800

    feat: 更新
    Signed-off-by: stanleyguo0207 <stanleyguo0207@163.com>

---
+++ learn-doc/src/lib.rs
@@ -0,0 +1,16 @@
+//! # Learn Doc
+//! `learn_doc` is learning document.
+
+/// Adds one to the number given.
+///
+/// # Examples
+///
+/// ```
+/// let arg = 5;
+/// let answer = learn_doc::add_one(arg);
+///
+/// assert_eq!(6, answer);
+/// ```
+pub fn add_one(x: i32) -> i32 {
+    x + 1
+}
---
+++ learn-doc/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    println!("Hello, world!");
+
+    let arg = 5;
+    let answer = learn_doc::add_one(arg);
+    println!("answer:{answer}")
+}
